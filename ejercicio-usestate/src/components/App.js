9659e5425d00641042dcfe7c152d2b2a948c176a
dayana <dayana.romero88@gmail.com>
2023-06-07 17:11:30 +0200

    Ejercicio formuario, con usestate, eventos, y filtro

---
+++ ejercicio-usestate/src/components/App.js
@@ -0,0 +1,33 @@
+import '../styles/App.css';
+import data from '../services/data.json';
+import Listproduct from './ListProduct/ListProduct';
+import { useState } from 'react';
+import Filter from './Filter/Filter';
+
+function App() {
+  const [products, setProducts] = useState(data);
+  const [filterName, setFilterName] = useState('');
+
+  const handleFilterName = (value) => {
+    setFilterName(value);
+  };
+
+  const filteredProducts = products.filter((item) =>
+    item.nombre.toLowerCase().includes(filterName.toLowerCase())
+  );
+  return (
+    <div className="App">
+      <Filter handleFilter={handleFilterName} />
+      <Listproduct list={filteredProducts} />
+    </div>
+  );
+}
+
+export default App;
+/*
+1.- crear un json(fichero json con los datos)
+2.- Map del array de datos  (componente lista, componente para la tarjeta)
+3.- buscar input para el filtro(componente para el filtro)
+4.- var de estado buscador del filtro.
+5.- var de estado  para la lista de elementos--> array
+*/
